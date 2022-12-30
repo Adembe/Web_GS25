24eedcdc72e5bb5ee9eeb7aa9a6eb09a8aed7333
Adembe <96032079+Adembe@users.noreply.github.com>
2022-12-30 17:45:38 +0800

    all page html done 100%

--- js/all-product-card.js
+++ js/all-product-card.js
@@ -64,7 +64,6 @@ const ProductCardData = [
     name: "Chicken25 MIX box",
     price: "22,000₮",
   },
-<<<<<<< HEAD
   {
     src: "img/product-img/product-chicken3.webp",
     alt: "chicken",
@@ -125,40 +124,6 @@ const ProductCardData = [
     name: "Халбага зөгийн бал 10Гр",
     price: "1,600₮",
   },
-
-  
-=======
-  // {
-  //   src: "img/product-img/product-chicken3.webp",
-  //   alt: "chicken",
-  //   name: "Chicken25 /sauce/",
-  //   price: "4,900₮",
-  // },
-  // {
-  //   src: "img/product-img/product-pepsi1.webp",
-  //   alt: "pepsi",
-  //   name: "Pepsi аягатай",
-  //   price: "1,600₮",
-  // },
-  // {
-  //   src: "img/product-img/product-coffee1.webp",
-  //   alt: "coffee",
-  //   name: "Кафе25 Aмерикано",
-  //   price: "3,500₮",
-  // },
-  // {
-  //   src: "img/product-img/product-mantuunbuuz1.webp",
-  //   alt: "mantuu",
-  //   name: "Мантуун бууз / кимчи /",
-  //   price: "1,800₮",
-  // },
-  // {
-  //   src: "img/product-img/product-boortsog.webp",
-  //   alt: "mantuu",
-  //   name: "Боорцог / мушгиа /",
-  //   price: "1,000₮",
-  // },
->>>>>>> e0ec5912df45b40bb5db9973be3e6d4eea33a2a3
 ];
 
 window.onload = () => {
